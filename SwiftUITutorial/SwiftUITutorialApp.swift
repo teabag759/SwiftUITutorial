fd0a9eca8aae266a51ee64de34dd34e1b05a23b2
Sohyeon Lee <teabag759@gmail.com>
2024-07-01 20:55:13 +0900

    Initial Commit

---
+++ SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by 이소현 on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
